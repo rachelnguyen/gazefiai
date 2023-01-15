03ca60468d1e7d12f43ac068a9529a21d022f58e
rachelnguyen <rachelthuy.nguyen@gmail.com>
2023-01-15 15:57:20 -0800

    Update script.js

--- client/script.js
+++ client/script.js
@@ -81,7 +81,7 @@ const handleSubmit = async (e) => {
 
   loader(messageDiv);
 
-  const response = await fetch('http://localhost:5001', {
+  const response = await fetch('https://gazefiai.onrender.com/', {
     method: 'POST',
     headers: {
       'Content-Type' : 'application/json', 
